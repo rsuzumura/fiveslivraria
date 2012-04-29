8ef1370cec8c58076a3f43bc37f1950faef7843c
shikamarujunior@gmail.com <shikamarujunior@gmail.com@0a17c803-17cb-8a32-2cae-dc855804c45f>
2012-04-29 19:09:16 +0000

    Classes que envolvem a gravação do pedido e geração dos documentos comprobatórios.

---
+++ FivesLivraria/FivesLivraria.Data/Utils/NotaFiscal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FivesLivraria.Data.Classes
+{
+    class NotaFiscal
+    {
+        public Pedido pedido { set; get; }
+        public ItemPedido item { set; get; }
+        public String linhaImpressao { set; get; }
+        
+        public void imprimeNota()
+        {
+        }
+
+        public void cancelaNota()
+        {
+        }
+
+        public void leituraX()
+        {
+        }
+
+        public void reducaoZ()
+        {
+        }
+
+        public void imprimirLinha()
+        {
+        }
+
+    }
+}
