d247b02cb706a5ea8ef6b77da601ec55d81ebda1
shikamarujunior@gmail.com <shikamarujunior@gmail.com@0a17c803-17cb-8a32-2cae-dc855804c45f>
2012-04-30 19:35:51 +0000

    solução de conflito...

--- FivesLivraria/FivesLivraria.Data/Utils/SimulaTEF.cs
+++ FivesLivraria/FivesLivraria.Data/Utils/SimulaTEF.cs
@@ -5,43 +5,57 @@ using System.Text;
 
 namespace FivesLivraria.Data.Classes
 {
-    public class SimulaTEF
-    {
-        public long numeroCartao { get; set; }
-        public int codigoVerificador { get; set; }
-        public bool statusAprovacao { get; set; }
+   public class SimulaTEF
+   {
+      private long numeroCartao;
+         public void setNumeroCartao( long numeroCartao )
+            { this.numeroCartao = numeroCartao; }
+         public long getNumeroCartao()
+            { return numeroCartao;  }
 
-        public SimulaTEF(long numeroCartao, int codigoVerificador, bool statusAprovacao)
-        {
+      private int codigoVerificador;
+         public void setCodigoVerificador( int codigo )
+            { this.codigoVerificador = codigo; }
+         public int getCodigoVerificador()
+            { return codigoVerificador; }
+      
+      private bool statusAprovacao;
+         public void setStatus( bool status)
+            { this.statusAprovacao = status; }
+         public bool isAprovado()
+            { return statusAprovacao; }
 
-        }
+      public SimulaTEF(long numeroCartao, int codigoVerificador, bool statusAprovacao)
+      {
 
-        public bool verificarPasta()
-        {
-            return true;
-        }
+      }
 
-        protected bool avaliarCredito()
-        {
-            return true;
-        }
+      public bool verificarPasta()
+      {
+         return true;
+      }
 
-        public void buscarDados()
-        {
+      protected bool avaliarCredito()
+      {
+         return true;
+      }
 
-        }
+      public void buscarDados()
+      {
 
-        public void gravarStatus()
-        {
-        }
+      }
 
-        public void transmitirResposta()
-        {
-        }
+      public void gravarStatus()
+      {
+      }
 
-        protected bool validarDados()
-        {
-            return true;
-        }
-    }
+      public void transmitirResposta()
+      {
+      }
+
+      protected bool validarDados()
+      {
+         return true;
+      }
+   }
 }
