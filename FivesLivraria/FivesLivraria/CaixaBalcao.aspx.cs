6641c1e61091d646bd8f14460e42b18bec90eaa0
shikamarujunior@gmail.com <shikamarujunior@gmail.com@0a17c803-17cb-8a32-2cae-dc855804c45f>
2012-04-30 05:50:49 +0000

    Inicio do desenvolvimento da emissao de cupom e comprovante de TEF.
    Janela CaixaBalcao...

--- FivesLivraria/FivesLivraria/CaixaBalcao.aspx.cs
+++ FivesLivraria/FivesLivraria/CaixaBalcao.aspx.cs
@@ -5,13 +5,39 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// bibliotecas específicas do projeto
+using FivesLivraria.Data.Classes;
+
 namespace FivesLivraria
 {
     public partial class CaixaBalcao : System.Web.UI.Page
     {
+       protected static string[,] ProdutosTeste = new string[5,3]
+                                 { {"1", "Produto 01", "25,00" },
+                                   {"2", "Produto 02", "15,00" },
+                                   {"3", "Produto 03", "35,00" },
+                                   {"4", "Produto 04", "20,00" },
+                                   {"5", "Produto 05", "10,00" } };
+       ItemPedido item;
+       int qtde;
+        area_TEF
+
         protected void Page_Load(object sender, EventArgs e)
         {
+           item = new ItemPedido();
+           qtde = 0;
+        }
 
+        protected void clk_itemPedido()
+        {
+           int codProd = int.Parse(ListFrmPgto.SelectedValue);
+           double vlr = double.Parse(ProdutosTeste[(codProd-1),3]);
+           string nome = ProdutosTeste[(codProd-1),2];
+
+           item.AddProduto(codProd, nome, vlr);
+           qtde++;
+
+           area_TEF.text = area_TEF.text + "\n" + item.toString(qtde);
         }
     }
 }
--- FivesLivraria/FivesLivraria/CaixaBalcao.aspx.designer.cs
+++ FivesLivraria/FivesLivraria/CaixaBalcao.aspx.designer.cs
@@ -8,9 +8,26 @@
 //------------------------------------------------------------------------------
 
 namespace FivesLivraria {
-
-
-    public partial class CaixaBalcao
-    {
+    
+    
+    public partial class CaixaBalcao {
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// ListFrmPgto control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList ListFrmPgto;
     }
 }
