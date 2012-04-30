0fce54acef4f2e510e822f3aaa9d534058f9e346
shikamarujunior@gmail.com <shikamarujunior@gmail.com@0a17c803-17cb-8a32-2cae-dc855804c45f>
2012-04-30 21:09:29 +0000

    dados iniciais para a página do balcão.

--- FivesLivraria/FivesLivraria.Data/Classes/Pedido.cs
+++ FivesLivraria/FivesLivraria.Data/Classes/Pedido.cs
@@ -8,20 +8,24 @@ namespace FivesLivraria.Data.Classes
    public class Pedido
    {
       private FormaPagamento pagamento;
-      private NotaFiscal notaFiscal;
+         public void setPagamento(int id, double valor)
+            {
+               if (id != FormaPagamento.PAGAMENTODINHEIRO)
+                  this.pagamento = new TEF(id, valor);
+               else
+                  this.pagamento = new FormaPagamento(id, valor);
+            }
+
+         public FormaPagamento getPagamento()
+            { return pagamento; }
+
+      private int codigoPedido;
+      private double totalPedido;
 
       public Pedido()
       {
-         //
-         // TODO: Add constructor logic here
-         //
-      }
-
-      public void setPagamento(int id, double valor)
-      {
-         //if (id != FormaPagamento.PAGAMENTODINHEIRO)
-           // this.pagamento = new TEF(id, valor);
 
       }
+
    }
 }
