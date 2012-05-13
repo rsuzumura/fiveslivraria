a2c2a913feb58272b198bc4b1026f54b056a81dc
shikamarujunior@gmail.com <shikamarujunior@gmail.com@0a17c803-17cb-8a32-2cae-dc855804c45f>
2012-05-13 22:41:04 +0000

    implementação dos modelos para simulação dos comprovantes TEF e Cupom Fiscal.

--- trunk/FivesLivraria/FivesLivraria.Data/Classes/TEF.cs
+++ trunk/FivesLivraria/FivesLivraria.Data/Classes/TEF.cs
@@ -7,12 +7,15 @@ namespace FivesLivraria.Data.Classes
 {
    public class TEF : FormaPagamento
    {
+      public const string linhaDiv = "==================================================";
+      public const string CNPJ = "11.222.333/0001-99";
       
       public long numeroCartao { get; set; }
       public int codigoCartao { get; set; }
       public double valorTransacao { get; set; }
       public bool statusConexao { get; set; }
       public long codigoCupom { get; set; }
+      public long codigoAutorizacao { get; set; }
 
       public TEF()
       { 
@@ -52,7 +55,7 @@ namespace FivesLivraria.Data.Classes
             txtImprimir += "CRED FACIL" + '\n';
             txtImprimir += "        Av. Paulista, 534, São Paulo" + '\n';
             txtImprimir += "term: " + 0001 + "         doc: " + codigoCupom.ToString() + "     lote: " + 0000 + '\n';
-            txtImprimir += DateTime.Today.ToString() + " " + DateTime.Now.ToString() + "     aut: " + 000000 + "     onl-X" + '\n';
+            txtImprimir += DateTime.Today.ToString() + " " + DateTime.Now.ToString() + "     aut: " + codigoAutorizacao.ToString() + "     onl-X" + '\n';
             txtImprimir += "venda " + operacao + "  a vista" + '\n';
             txtImprimir += "valor venda:                " + valorTransacao.ToString() + '\n';
             txtImprimir += '\n';
@@ -60,10 +63,23 @@ namespace FivesLivraria.Data.Classes
 
             txtImprimir += "EMULADOR DE ECF                     " + /* usuariologado +*/ '\n';
             txtImprimir += "V. 001                              " + /* id maquina + */ '\n';
-            txtImprimir += '\n';
-            txtImprimir += '\n';
-            txtImprimir += '\n';
-            txtImprimir += '\n';
+            txtImprimir += "                           " + DateTime.Today.ToString() + " " + DateTime.Now.ToString() + '\n';
+
+            txtImprimir += linhaDiv + '\n';
+            txtImprimir += "                2ª via - cliente" + '\n';
+            txtImprimir += linhaDiv + '\n';
+
+            txtImprimir += "        comprovante de " + operacao + '\n';
+            txtImprimir += "CRED FACIL" + '\n';
+            txtImprimir += "CNPJ: " + CNPJ + '\n';
+            txtImprimir += "" + '\n';
+            txtImprimir += Comprovante.cabecalho();
+            txtImprimir += "" + '\n';
+
+            txtImprimir += "term: " + 0001 + "         doc: " + codigoCupom.ToString() + "     lote: " + 0000 + '\n';
+            txtImprimir += DateTime.Today.ToString() + " " + DateTime.Now.ToString() + "     aut: " + codigoAutorizacao.ToString() + "     onl-X" + '\n';
+            txtImprimir += "venda " + operacao + "  a vista" + '\n';
+            txtImprimir += "valor venda:                " + valorTransacao.ToString() + '\n';
 
          return txtImprimir;
       }
