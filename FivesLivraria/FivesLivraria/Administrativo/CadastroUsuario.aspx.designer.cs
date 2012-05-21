829423823bed1aa1e5b65ea2e4b6a39a9a4f3553
ricardosuzumura@gmail.com <ricardosuzumura@gmail.com@0a17c803-17cb-8a32-2cae-dc855804c45f>
2012-05-21 13:10:58 +0000

    Inclusão do resumo de validação do cadastro do usuário (administrativo)

--- FivesLivraria/FivesLivraria/Administrativo/CadastroUsuario.aspx.designer.cs
+++ FivesLivraria/FivesLivraria/Administrativo/CadastroUsuario.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace FivesLivraria.Administrativo {
         protected global::System.Web.UI.WebControls.Button btnCancel;
         
         /// <summary>
+        /// validationResume control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary validationResume;
+        
+        /// <summary>
         /// rfvUser control.
         /// </summary>
         /// <remarks>
